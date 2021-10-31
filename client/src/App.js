c774f5d8cd37551f8d2938d96dd5d1eee90e9796
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-10-31 15:57:11 -0400

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Creating a client object to use with the ApolloProvider to make every request work with the Apollo server
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={SearchBooks} />
+          <Route exact path='/saved' component={SavedBooks} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
